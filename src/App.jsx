022c95a3922156864dc661c99f3c05a495af7798
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2023-08-23 19:49:47 +0700

    create function calculateDownPayment and calculateDownPaymentFinal

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer';
@@ -6,10 +6,48 @@ import { BsFillQuestionCircleFill } from "react-icons/bs";
 
 function App() {
   const [duration, setDuration] = useState(0)
-  const [price, setPrice] = useState(0)
+  const [price, setPrice] = useState(null)
   const [dpPercentage, setDpPercentage] = useState(0)
   const [downPayment, setDownPayment] = useState(0)
-  const [inflationPercentage, setInflationPercentage] = useState(0)
+  const [inflationPercentage, setInflationPercentage] = useState(5)
+  const [downPaymentFinal, setDownPaymentFinal] = useState(0)
+
+  const calculateDownPayment = (price, dpPercentage) => {
+    return setDownPayment(price * (dpPercentage / 100))
+  }
+
+  const calculateDownPaymentFinal = (price, dpPercentage, duration, inflationPercentage) => {
+    let dpFinal = price;
+
+    for (let i = 0; i < duration; i++) {
+      dpFinal *= 1 + (inflationPercentage / 100);
+    }
+
+    dpFinal *= dpPercentage / 100;
+    return setDownPaymentFinal(dpFinal);
+  }
+
+  const formatToRupiah = (number) => {
+    return new Intl.NumberFormat('id-ID', {
+      currency: 'IDR',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0
+    }).format(number);
+  }
+
+  const handleNumberChange = (e) => {
+    const inputValue = e.target.value;
+    const numericInput = inputValue.replace(/[^0-9]/g, '');
+    setPrice(numericInput);
+  };
+
+  useEffect(() => {
+    calculateDownPayment(price, dpPercentage)
+    calculateDownPaymentFinal(price, dpPercentage, duration, inflationPercentage)
+    console.log(downPayment)
+    console.log(downPaymentFinal)
+  }, [duration, price, dpPercentage, downPayment, inflationPercentage, downPaymentFinal])
+
 
   return (
     <>
@@ -23,7 +61,7 @@ function App() {
             </div>
           </label>
           <label className="input-group">
-            <input type="text" className="input input-bordered w-full" />
+            <input type="text" className="input input-bordered w-full" onInput={(e) => setDuration(e.target.value)} />
             <span className=''>Years</span>
           </label>
         </div>
@@ -34,17 +72,20 @@ function App() {
           </label>
           <label className="input-group">
             <span>Rp</span>
-            <input type="text" className="input input-bordered w-full" />
+            <input type="text" className="input input-bordered w-full" value={price} onInput={(e) => setPrice(e.target.value)} onChange={handleNumberChange} />
           </label>
         </div>
 
         <div className="form-control w-full mb-4">
           <label className="label">
-            <span className="label-text">% down payment you want to pay of</span>
+            <span className="label-text">% Down Payment you want to pay of</span>
           </label>
           <label className="input-group">
-            <input type="text" className="input input-bordered w-full" />
+            <input type="text" className="input input-bordered w-full" value={dpPercentage} onInput={(e) => setDpPercentage(e.target.value)} />
             <span>%</span>
+          </label>
+          <label className="label">
+            <span className="label-text-alt">*Input 100% if you want to pay in full.</span>
           </label>
         </div>
 
@@ -54,30 +95,33 @@ function App() {
           </label>
           <label className="input-group">
             <span>Rp</span>
-            <input type="text" className="input input-bordered  w-full" />
+            <input type="text" className="input input-bordered w-full" value={downPayment} disabled />
           </label>
         </div>
 
         <div className="form-control w-full mb-4">
           <label className="label">
             <span className="label-text">Property inflation assumptions are</span>
+            <div className="tooltip" data-tip="Inflation can be defined as a general and continuous increase in the prices of goods and services over a period of time.">
+              <BsFillQuestionCircleFill />
+            </div>
           </label>
           <label className="input-group">
-            <input type="text" className="input input-bordered w-full" />
+            <input type="text" className="input input-bordered w-full" value={inflationPercentage} onInput={(e) => setInflationPercentage(e.target.value)} />
             <span>%/Year</span>
           </label>
         </div>
 
         <div className="form-control w-full mb-4">
           <label className="label">
-            <span className="label-text">The total money you will need in 5 years to pay the down payment is</span>
+            <span className="label-text">The total money you will need in {duration} years to pay the down payment is</span>
             <div className="tooltip" data-tip="Due to inflation every year, this affects the price of your dream property.">
               <BsFillQuestionCircleFill />
             </div>
           </label>
           <label className="input-group">
             <span>Rp</span>
-            <input type="text" className="input input-bordered  w-full" />
+            <input type="text" className="input input-bordered w-full" value={formatToRupiah(downPaymentFinal.toFixed(0))} disabled />
           </label>
         </div>
       </div>
